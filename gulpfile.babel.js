38368e7a09f927c2f58c818af662ea324c21072b
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2020-01-17 19:36:51 +0530

    First Commit, and HOme banner done.

---
+++ gulpfile.babel.js
@@ -0,0 +1,232 @@
+/**
+ * Gulpfile
+ * Author: Sriram Kasyap Meduri
+ * Date: 24th Jan, 2019
+ * Required Gulp Version: 4.0.0
+ *
+ * Description: Gulp file to Compile SASS/SCSS, Transpile, Bundle and Minify JS files, then serve them via browserSync.
+ */
+
+/**
+ * Project Constants
+ *
+ * Change the project name and URL as require. No need to change others unless absolutely required.
+ */
+"use strict";
+
+var project = "samskruti",
+    projectURL = `http://localhost/${project}/`,
+    styleSRC = "src/scss",
+    styleDestination = "dist/css",
+    scriptSRC = "src/js",
+    scriptDestination = "dist/js",
+    imgSRC = "img",
+    imgDest = "dist/img",
+    phpWatchFiles = "**/*.php",
+    htmlWatchFiles = "**/*.html";
+
+/**
+ * Load Plugins
+ */
+import gulp from "gulp";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import sourcemaps from "gulp-sourcemaps";
+import rename from "gulp-rename";
+import notify from "gulp-notify";
+import minify from "gulp-minify";
+import babel from "gulp-babel";
+import filter from "gulp-filter";
+import imagemin from "gulp-imagemin";
+
+/**
+ * These plugins are old school cool
+ */
+var lec = require("gulp-line-ending-corrector"),
+    mmq = require("gulp-merge-media-queries"),
+    browserSync = require("browser-sync").create();
+
+/**
+ * Browsers to be targeted by Autoprefixer
+ */
+const AUTOPREFIXER_BROWSERS = [
+    "last 2 version",
+    "> 1%",
+    "ie >= 9",
+    "ie_mob >= 10",
+    "ff >= 30",
+    "chrome >= 34",
+    "safari >= 7",
+    "opera >= 23",
+    "ios >= 7",
+    "android >= 4",
+    "bb >= 10"
+];
+
+/**
+ * Initialize the BrowserSync. It's the Key!
+ */
+const browserSyncStart = done => {
+    browserSync.init({
+        proxy: projectURL,
+        open: true,
+        injectChanges: true
+    });
+    done();
+};
+
+/**
+ * Task to reload Browser using BrowserSync
+ */
+const reload = done => {
+    browserSync.reload();
+    done();
+};
+
+/**
+ * Compile SASS/SCSS files to single style.min.css file,
+ * Autoprefix for Cross-Browser compatibility,
+ * merge multiple media queries,
+ * Correct line endings, and
+ * Inject the changes into the browser
+ */
+const compileSass = done => {
+    gulp.src(`${styleSRC}/**/*.scss`)
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(
+            sass({
+                errLogToConsole: true,
+                outputStyle: "compressed",
+                sourceComments: "map",
+                sourceMap: "sass"
+            })
+        )
+        .on("error", console.error.bind(console))
+        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest(styleDestination))
+        .pipe(filter("**/*.css"))
+        .pipe(mmq({ log: true }))
+        .pipe(lec())
+        .pipe(browserSync.stream())
+        .pipe(notify("Stylesheets Compiled! 💯"));
+    done();
+};
+
+/**
+ * Transpile Javascript (or ES6 if you wanna be super cool),
+ * minify it and put it in it's place (Script Destination, obviously)
+ */
+const compileJS = done => {
+    gulp.src(`${scriptSRC}/**/*.js`)
+        .pipe(babel())
+        .pipe(
+            minify({
+                ext: {
+                    min: ".js"
+                }
+            })
+        )
+        .pipe(gulp.dest(scriptDestination))
+        .pipe(notify("Scripts Compiled! 💯"));
+    done();
+};
+
+/**
+ * Optimize images, because who wants a slow web page?
+ * Then also show them their place.
+ */
+const optimizeImg = done => {
+    gulp.src(`${imgSRC}/*`)
+        .pipe(
+            imagemin({
+                progressive: true
+            })
+        )
+        .pipe(gulp.dest(imgDest))
+        .pipe(notify({ message: "Images optimized! 💯", onLast: true }));
+    done();
+};
+
+/**
+ * Watch Sass files in Style Sources folder for changes and Compile.
+ */
+const watchSass = done => {
+    gulp.watch(`${styleSRC}/**/*.scss`, compileSass);
+    done();
+};
+
+/**
+ * Watch Javascript files for changes and compile them if they do.
+ */
+const watchJS = done => {
+    gulp.watch(`${scriptSRC}/**/*.js`, gulp.series(compileJS, reload));
+    done();
+};
+
+/**
+ * Watch images for changes and Optimize the hell out of them
+ */
+const watchImg = done => {
+    gulp.watch(`${imgSRC}/*`, optimizeImg);
+    done();
+};
+
+/**
+ * Life is too short to keep hitting F5 and Cmd + R
+ * Watch HTML Files for changes and reload the web page when they do.
+ */
+const watchHTML = done => {
+    gulp.watch(htmlWatchFiles, reload);
+    done();
+};
+
+/**
+ * Life is still Short..
+ * Watch PHP files for changes and reload the web page when they do.
+ */
+const watchPHP = done => {
+    gulp.watch(phpWatchFiles, reload);
+    done();
+};
+
+/**
+ * Watch Image destination folder for new optimized images and reload the web page.
+ */
+const watchDistImg = done => {
+    gulp.watch(imgDest, reload);
+    done();
+};
+
+/**
+ * Make all the watch tasks to watch over us from this one single 'watch'.
+ */
+const watch = gulp.parallel(
+    watchSass,
+    watchJS,
+    watchPHP,
+    watchHTML,
+    watchImg,
+    watchDistImg
+);
+
+/**
+ * Compile the s**t out of all the compiling tasks with one compile command
+ */
+const compile = gulp.parallel(compileSass, compileJS, optimizeImg);
+
+/**
+ * The File is ready for Gulp(ing). 'serve' it hot
+ */
+const serve = gulp.series(compile, browserSyncStart);
+
+/**
+ * The Default Task. The Start Switch. The first domino
+ */
+const defaultTask = gulp.parallel(serve, watch);
+
+/**
+ * Let's start Shippin'
+ */
+export { compile, serve, watch };
+export default defaultTask;

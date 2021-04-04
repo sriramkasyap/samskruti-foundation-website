3c613ae39de113dbbefdec0d4e3b7cd4e5f82abb
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-04-04 13:16:08 +0530

    story section done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+function EasyPeasyParallax() {
+    var scrollPos = jQuery(document).scrollTop();
+    var targetOpacity = 1;
+    var flag = false;
+    if (scrollPos < 1) {
+        targetOpacity = "0";
+    } else {
+        targetOpacity = scrollPos * 2 / window.innerHeight;
+        flag = true;
+    }
+    // targetOpacity;
+    jQuery("nav").css({
+        "background-color": "rgba(41, 41, 41, " + targetOpacity + ")"
+    });
+
+    if (flag) {
+        jQuery("nav").addClass("turn-white");
+    } else {
+        jQuery("nav").removeClass("turn-white");
+    }
+}
+
+$(function () {
+    $(window).scroll(function () {
+        EasyPeasyParallax();
+    });
+});
